80fb81a7b6f88d207cf057b7d4caa4c02cb47818
Jan Škrášek <hrach.cz@gmail.com>
2009-06-06 20:31:29 +0000

    - adds svn:keywords

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  *
  * @author      Jan Skrasek <hrach.cz@gmail.com>
  * @copyright   Copyright (c) 2008 - 2009, Jan Skrasek
- * @version     0.6.7
+ * @version     0.7 $Id$
  * @link        http://phob.skrasek.com
  * @package     Phob
  */
